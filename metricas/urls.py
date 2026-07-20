a99901e33ba5528d34e0ddce98d1ebeaf019dfca
EDGARMOSS1 <edgar.cabreram@my.unitec.edu.mx>
2026-07-20 13:37:29 -0600

    Agregar analisis automatico de resultados

--- metricas/urls.py
+++ metricas/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('metricas/', views.dashboard_metricas, name='dashboard_metricas'),
     path('metricas/exportar-csv/', views.exportar_metricas_csv, name='exportar_metricas_csv'),
+    path('metricas/analisis/', views.analisis_resultados, name='analisis_resultados'),
 ]
--- metricas/views.py
+++ metricas/views.py
@@ -1,7 +1,8 @@
 import csv
 from django.shortcuts import render
 from django.http import HttpResponse
-from pedidos.models import Pedido
+from django.db.models import Sum
+from pedidos.models import Pedido, DetallePedido
 
 
 def dashboard_metricas(request):
@@ -78,3 +79,60 @@ def exportar_metricas_csv(request):
         ])
 
     return response
+
+
+def analisis_resultados(request):
+    pedidos = Pedido.objects.all().order_by('-fecha_creacion')
+    pedidos_entregados = pedidos.filter(estado=Pedido.Estado.ENTREGADO)
+
+    total_pedidos = pedidos.count()
+    total_entregados = pedidos_entregados.count()
+
+    venta_total = sum(pedido.total for pedido in pedidos)
+    venta_promedio = 0
+
+    if total_pedidos > 0:
+        venta_promedio = round(venta_total / total_pedidos, 2)
+
+    tiempos_atencion = []
+    pedido_mas_rapido = None
+    pedido_mas_lento = None
+    menor_tiempo = None
+    mayor_tiempo = None
+
+    for pedido in pedidos_entregados:
+        tiempo = pedido.tiempo_total_minutos()
+
+        if tiempo is not None:
+            tiempos_atencion.append(tiempo)
+
+            if menor_tiempo is None or tiempo < menor_tiempo:
+                menor_tiempo = tiempo
+                pedido_mas_rapido = pedido
+
+            if mayor_tiempo is None or tiempo > mayor_tiempo:
+                mayor_tiempo = tiempo
+                pedido_mas_lento = pedido
+
+    promedio_atencion = 0
+    if tiempos_atencion:
+        promedio_atencion = round(sum(tiempos_atencion) / len(tiempos_atencion), 2)
+
+    producto_mas_vendido = DetallePedido.objects.values(
+        'producto__nombre'
+    ).annotate(
+        total_vendido=Sum('cantidad')
+    ).order_by('-total_vendido').first()
+
+    return render(request, 'metricas/analisis_resultados.html', {
+        'total_pedidos': total_pedidos,
+        'total_entregados': total_entregados,
+        'venta_total': venta_total,
+        'venta_promedio': venta_promedio,
+        'promedio_atencion': promedio_atencion,
+        'pedido_mas_rapido': pedido_mas_rapido,
+        'pedido_mas_lento': pedido_mas_lento,
+        'menor_tiempo': menor_tiempo,
+        'mayor_tiempo': mayor_tiempo,
+        'producto_mas_vendido': producto_mas_vendido,
+    })
